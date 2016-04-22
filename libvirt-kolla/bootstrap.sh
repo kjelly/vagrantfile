f03029a11a5a2529139d11e1d808ff1efff60427
Kuo-tung Kao <ya790206@gmail.com>
2016-04-22 20:35:55 +0800

    update

--- libvirt-kolla/bootstrap.sh
+++ libvirt-kolla/bootstrap.sh
@@ -1,4 +1,10 @@
+export LC_ALL=C
 cd /vagrant/kolla
+echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" >> /etc/apt/sources.list
+sudo apt-get update
+sudo apt-get install -y --force-yes python-setuptools git docker-engine
+sudo easy_install pip
 python setup.py install
 pip install -r requirements.txt
 pip install ansible==1.9.2
+pip install tox
